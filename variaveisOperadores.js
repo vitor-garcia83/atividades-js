8641cb934c1f8d6bd883b51925d8f774286b5c86
Vitor Garcia <83vitorhugoooo@gmail.com>
2025-03-06 19:48:11 -0300

    Atividade 1

---
+++ variaveisOperadores.js
@@ -0,0 +1,19 @@
+let num1 = 10, num2 = 5;
+
+let soma = num1 + num2;
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+
+let subtracao = num1 - num2;
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+
+let multiplicacao = num1 * num2;
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+
+let divisao = num1 / num2;
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
+
+let modulo = num1 % num2;
+console.log(`Resultado do módulo entre ${num1} e ${num2} é ${modulo}`);
+
+let exponenciacao = num1 ** num2;
+console.log(`Resultado da exponenciação entre ${num1} e ${num2} é ${exponenciacao}`);
